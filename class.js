df08a4a87d2357653f514cb79a1a25031681b1a9
BennyInokp <benedictaakhilome@email.com>
2023-03-29 22:58:36 +0100

    class work done

--- class.js
+++ class.js
@@ -7,17 +7,61 @@
 //using object.value and the for...of loop.
 //if salaries is empty, then the result must be 0.
 
-let salaries = {
-  john: 100,
-  pete: 300,
-  mary: 250,
+//let salaries = {
+//john: 100,
+//pete: 300,
+//mary: 250,
+//};
+
+//function sumSalaries(salaries) {
+//let sum = 0;
+//for (let salary of Object.values(salaries)) {
+//  sum += salary;
+//}
+// return sum;
+//}
+//console.log(sumSalaries(salaries))
+
+//There is a salaries object:
+/*let salaries = {
+  "John":100,
+  "Pete":300,
+  "Mary":250
 };
 
-function sumSalaries(salaries) {
-  let sum = 0;
-  for (let salary of Object.values(salaries)) {
-    sum += salary;
-  } 
-  return sum;
-}
-console.log(sumSalaries(salaries))
+*/
+
+//create the function topSalary(salaries) that returns the name of the top-paid person
+//if there are multiple top-paid Persons , return any of them.
+//P.S. use Object.enteries and destructuring to iterate over key/value pairs
+
+//function topSalary(salaries){
+//let sum = 0
+// for (let [key, value ] of Object.enteries(salaries)){
+// if (top-paid < 300);
+//  return Name
+// }
+// elseif(top-paid >= 300);
+// return Name
+// {
+
+// }
+//}
+//console.log(topSalary(salaries));
+
+let user = {
+  name: "john",
+  years: 30,
+  isAdmin: "false",
+};
+//name property into the variable name
+//years property into the variable age.
+//isAdmit property into the variable isAdmin(false,if no such property)
+
+let name = user.name;
+let age = user.years;
+let isAdmin = user.isAdmin;
+
+console.log(name);
+console.log(age);
+console.log(isAdmin);
